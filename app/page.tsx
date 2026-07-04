275dfca1c730b10f3797f9a2f59be03d94a4e18c
KPulathisi <mdkpjayashan@gmail.com>
2026-07-04 10:53:54 +0530

    cinflict 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import { AnnouncementBar } from "@/components/ui/AnnouncementBar";
-import { Navbar } from "@/components/features/navbar/Navbar";
 import { HeroSection } from "@/components/features/hero/HeroSection";
 import { StoryHeritage } from "@/components/features/story/StoryHeritage";
 import { BestSellers } from "@/components/features/products/BestSellers";
@@ -7,26 +5,20 @@ import { ShopByCategory } from "@/components/features/categories/ShopByCategory"
 import { TrustBadges } from "@/components/ui/TrustBadges";
 import { GiftSetsBanner } from "@/components/features/gifts/GiftSetsBanner";
 import { Testimonials } from "@/components/features/testimonials/Testimonials";
-import { Footer } from "@/components/features/footer/Footer";
 import { ScrollRevealWrapper } from "@/components/ui/ScrollRevealWrapper";
 
 export default function HomePage() {
   return (
     <>
-      <AnnouncementBar />
-      <Navbar />
-      <main className="flex-1">
-        <HeroSection />
-        <ScrollRevealWrapper>
-          <StoryHeritage />
-          <BestSellers />
-          <ShopByCategory />
-          <TrustBadges />
-          <GiftSetsBanner />
-          <Testimonials />
-        </ScrollRevealWrapper>
-      </main>
-      <Footer />
+      <HeroSection />
+      <ScrollRevealWrapper>
+        <StoryHeritage />
+        <BestSellers />
+        <ShopByCategory />
+        <TrustBadges />
+        <GiftSetsBanner />
+        <Testimonials />
+      </ScrollRevealWrapper>
     </>
   );
 }
